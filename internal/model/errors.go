fabbfa90431c945693f8c07fe88efbc6b6561013
raykhonakhikmatullayeva <raya0106777@gmail.com>
2026-07-19 13:24:35 +0500

    feat: fixed main godotenv.Load() (#2)
    * feat: fixed main godotenv.Load()
    
    * feat: added Sentinel-ошибки
    
    * created env file

--- internal/model/errors.go
+++ internal/model/errors.go
@@ -1,1 +1,11 @@
 package model
+
+import "errors"
+
+var (
+	ErrNotFound  = errors.New("record not found")
+	ErrInvalid   = errors.New("invalid input")
+	ErrForbidden = errors.New("forbidden")
+	ErrOutbid    = errors.New("the bid is lower than the current bid")
+	ErrClosed    = errors.New("lot closed")
+)
